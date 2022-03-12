db1a6231263d85b82beaf6d0f39ec2702c9e04f4
Florian Klink <flokli@flokli.de>
2022-03-12 12:17:29 +0100

    pkg/util/helpers_test.go: move into util_test package

--- pkg/util/helpers_test.go
+++ pkg/util/helpers_test.go
@@ -1,8 +1,9 @@
-package util
+package util_test
 
 import (
 	"testing"
 
+	"github.com/flokli/nix-casync/pkg/util"
 	"github.com/numtide/go-nix/nixbase32"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,13 +12,13 @@ func TestStorePathHelpers(t *testing.T) {
 	storePath := "/nix/store/dr76fsw7d6ws3pymafx0w0sn4rzbw7c9-etc-os-release"
 
 	t.Run("getHashFromStorePath", func(t *testing.T) {
-		hash, err := GetHashFromStorePath(storePath)
+		hash, err := util.GetHashFromStorePath(storePath)
 		assert.NoError(t, err)
 		assert.Equal(t, "dr76fsw7d6ws3pymafx0w0sn4rzbw7c9", nixbase32.EncodeToString(hash))
 	})
 
 	t.Run("getNameFromStorePath", func(t *testing.T) {
-		name := GetNameFromStorePath(storePath)
+		name := util.GetNameFromStorePath(storePath)
 		assert.Equal(t, "etc-os-release", name)
 	})
 }
